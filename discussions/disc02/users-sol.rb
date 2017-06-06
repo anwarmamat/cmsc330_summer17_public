535905d8b5ebea5354fcfe655c7ba2d8c1195eb0
Joseph Bergman <bergmancoding@gmail.com>
2017-06-06 12:19:15 -0400

    Discussion 2 solution

--- discussions/disc02/users-sol.rb
+++ discussions/disc02/users-sol.rb
@@ -1,23 +1,68 @@
 class Person
+  attr_accessor :id, :first_name, :last_name, :phone, :email
+
   # Add an initializer to create a person using id, name, phone, and email
   def initialize(id,fname,lname,phone,email)
-    raise "Not implemented"
+    @id = id
+    @first_name = fname
+    @last_name = lname
+    @phone = phone
+    @email = email
   end
 end
 
 class Users
+  attr_accessor :users, :corrupted
+
   # Add initializer to store all users and the id of all corrupted users
   def initialize
-    raise "Not implemented"
+    @users = Hash.new
+    @corrupted = Array.new
   end
 
   # Parse the file to find all valid and corrupted users
-  def load(filename)
-    # Note: Must get a first name and last name
-    # Note: Phone should be formatted (###) ###-####
-    # Note: Assume you'll always have a valid id
-    # Return true
-    raise "Not implemented"
+  def load(file)
+    File.foreach(file) do |line|
+      user = line.split(',')
+      valid = true
+
+      # Match id
+      if user[0] =~ /\d+/ then
+        id = user[0].to_i
+      else
+        valid = false
+      end
+
+      # Match Name
+      if user[1] =~ /([a-zA-Z]+) ([a-zA-Z]+)/ then
+        fname = $1
+        lname = $2
+      else
+        valid = false
+      end
+
+      # Match Phone
+      if user[2] =~ /(\d{3}) (\d{3}) (\d{4})/ then
+        phone = "(#{$1}) #{$2}-#{$3}"
+      else
+        valid = false
+      end
+
+      # Match Email
+      if user[3] =~ /\w{3,}@\w{3,}\.[a-zA-Z]{2,}/ then
+        email = user[3].chomp
+      else
+        valid = false
+      end
+
+      # Add to Data Structure
+      if valid then
+        @users[id] = Person.new(id,fname,lname,phone,email)
+      else
+        @corrupted << id
+      end
+    end
+    return true
   end
 
 end
