01b5d8378667a2264920821d49252a42eaf4eb5b
Joseph Bergman <bergmancoding@gmail.com>
2017-06-06 02:24:38 -0400

    Discussion 2

---
+++ discussions/disc02/users.rb
@@ -0,0 +1,23 @@
+class Person
+  # Add an initializer to create a person using id, name, phone, and email
+  def initialize(id,fname,lname,phone,email)
+    raise "Not implemented"
+  end
+end
+
+class Users
+  # Add initializer to store all users and the id of all corrupted users
+  def initialize
+    raise "Not implemented"
+  end
+
+  # Parse the file to find all valid and corrupted users
+  def load(filename)
+    # Note: Must get a first name and last name
+    # Note: Phone should be formatted (###) ###-####
+    # Note: Assume you'll always have a valid id
+    # Return true 
+    raise "Not implemented"
+  end
+
+end
